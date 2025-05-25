10d7c6d4db036b77460a88d681ab066be941b8a1
汪少棠 <2268461750@qq.com>
2025-05-25 16:02:31 +0800

    项目初始化.

---
+++ src/main/java/com/wmx/jdk17app/Jdk17appApplication.java
@@ -0,0 +1,13 @@
+package com.wmx.jdk17app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk17appApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk17appApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wmx/jdk17app/Jdk17appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmx.jdk17app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk17appApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

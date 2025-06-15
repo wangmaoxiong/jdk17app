592a2f0b38f146dac459404bfbd46263c4f9b284
汪少棠 <2268461750@qq.com>
2025-06-15 08:48:47 +0800

    JXLS 测试模板文件

--- src/test/java/com/wmx/jdk17app/jxls3/test/HelloWorldTest.java
+++ src/test/java/com/wmx/jdk17app/jxls3/test/HelloWorldTest.java
@@ -28,17 +28,17 @@ public class HelloWorldTest {
     private static final Logger log = LoggerFactory.getLogger(HelloWorldTest.class);
 
     @Test
-    public void test() throws FileNotFoundException {
+    public void test() {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(1001, "张三", DateUtil.parse("1993-04-22"), new BigDecimal("18833.54")));
         employees.add(new Employee(1002, "李四", DateUtil.parse("1994-09-02"), new BigDecimal("28855.14")));
-        employees.add(new Employee(1003, "王五", DateUtil.parse("1995-08-11"), new BigDecimal("38988.50")));
+        employees.add(new Employee(1003, "王五", DateUtil.parse(null), new BigDecimal("38988.50")));
         employees.add(new Employee(1004, "Jon", DateUtil.parse("1992-01-16"), new BigDecimal("8868.48")));
         employees.add(new Employee(1005, "约翰", DateUtil.parse("1991-06-08"), new BigDecimal("7888.66")));
 
         InputStream templateInputStream = HelloWorldTest.class.getClassLoader().getResourceAsStream("jxls3/HelloWorld.xlsx");
         File homeDirectory = FileSystemView.getFileSystemView().getHomeDirectory();
-        File outputFile = new File(homeDirectory, "HelloWorld.xlsx");
+        File outputFile = new File(homeDirectory, System.currentTimeMillis() + ".xlsx");
         JxlsOutputFile jxlsOutputFile = new JxlsOutputFile(outputFile);
 
         Map<String, Object> data = new HashMap<>();

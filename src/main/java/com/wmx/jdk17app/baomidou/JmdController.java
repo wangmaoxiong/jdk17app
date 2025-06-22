d112b00a35660e715e159533a4a67848d7bdb675
汪少棠 <2268461750@qq.com>
2025-06-22 19:10:00 +0800

    基于springboot的快速集成多数据源的启动器

--- src/main/java/com/wmx/jdk17app/baomidou/JmdController.java
+++ src/main/java/com/wmx/jdk17app/baomidou/JmdController.java
@@ -1,5 +1,6 @@
 package com.wmx.jdk17app.baomidou;
 
+import com.baomidou.dynamic.datasource.toolkit.CryptoUtils;
 import com.wmx.jdk17app.pojo.ResultData;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import java.util.Map;
 /**
  * Springboot + JdbcTemplate + hikari 多数据源使用
  * 注入同一类型全部实现类的实例
+ * 参考文档：https://blog.csdn.net/co_zjw/article/details/144949823
  *
  * @author wangMaoXiong
  * @version 1.0
@@ -21,6 +23,25 @@ public class JmdController {
 
     @Resource
     private JmdService jmdService;
+
+    /**
+     * dynamic-datasource 项目支持url,username,password加密
+     * 使用RAS加密
+     * 简单来说就是生成两把钥匙，私钥加密，公钥解密。
+     * 公钥可以发布出去，用于项目启动时自动解密。
+     * 将生成public-key、以及加密的内容放在yml配置文件中即可
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        String[] arr = CryptoUtils.genKeyPair(512);
+        System.out.println("privateKey:  " + arr[0]);
+        System.out.println("publicKey:  " + arr[1]);
+        System.out.println("url:  ENC(" + CryptoUtils.encrypt(arr[0], "jdbc:h2:~/test_frame;AUTO_SERVER=TRUE") + ")");
+        System.out.println("username:  ENC(" + CryptoUtils.encrypt(arr[0], "admin") + ")");
+        System.out.println("password:  ENC(" + CryptoUtils.encrypt(arr[0], "123456") + ")");
+    }
 
     /**
      * 使用默认的数据源进行操作
@@ -75,6 +96,17 @@ public class JmdController {
     @GetMapping("jmd/h2FrameInit")
     public ResultData<List<Map<String, Object>>> h2FrameInit() {
         jmdService.h2FrameInit();
+        List<Map<String, Object>> mapList = jmdService.h2FrameLoadAllUser();
+        return ResultData.success(mapList);
+    }
+
+    /**
+     * http://localhost:8080/jmd/h2FrameLoadAllUser
+     *
+     * @return
+     */
+    @GetMapping("jmd/h2FrameLoadAllUser")
+    public ResultData<List<Map<String, Object>>> h2FrameLoadAllUser() {
         List<Map<String, Object>> mapList = jmdService.h2FrameLoadAllUser();
         return ResultData.success(mapList);
     }
